dc776e7ab18b8d88b759547214fdcfe47ccec160
Bruno Normande Lins <normandelins@gmail.com>
2016-02-22 18:38:54 -0300

    Adicionando obstáculo dentro da região de busca

--- src/ParticleSystem.cpp
+++ src/ParticleSystem.cpp
@@ -174,6 +174,7 @@ void ParticleSystem::createParticles(){
 			float obstacle_radius = side*params.particle_radius;
 			y0 = side*params.particle_radius*2 + params.particle_radius ;
 
+			// todo: create addObstacle method
 			hObs[0] = make_float4(obstacle_radius);
 			add_to_max_border.x = side*params.particle_radius*10;
 			add_to_max_border.y = side*params.particle_radius*10;
@@ -187,6 +188,21 @@ void ParticleSystem::createParticles(){
 	}
 
 	distributeParticles(grid_size, distance, jitter, y0);
+
+	//TODO: Create method to define system boundaries
+	if(params.p_max.x < hObs[0].x + hObs[0].w)
+		params.p_max.x = hObs[0].x + hObs[0].w;
+	if(params.p_max.y < hObs[0].y + hObs[0].w)
+			params.p_max.y = hObs[0].y + hObs[0].w;
+	if(params.p_max.z < hObs[0].z + hObs[0].w)
+			params.p_max.z = hObs[0].z + hObs[0].w;
+
+	if(params.p_min.x > hObs[0].x - hObs[0].w)
+		params.p_min.x = hObs[0].x - hObs[0].w;
+	if(params.p_min.y > hObs[0].y - hObs[0].w)
+			params.p_min.y = hObs[0].y - hObs[0].w;
+	if(params.p_min.z > hObs[0].z - hObs[0].w)
+			params.p_min.z = hObs[0].z - hObs[0].w;
 
 	params.p_max += add_to_max_border;
 	params.p_min -= add_to_max_border;
