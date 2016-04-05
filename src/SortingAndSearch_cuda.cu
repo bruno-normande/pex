ae2fd4adb35166e9a7fe85bd89d2a16500d148df
Bruno Normande Lins <normandelins@gmail.com>
2016-04-05 13:01:14 -0300

    Implementando busca binária para SortingAndSearch

--- src/SortingAndSearch_cuda.cu
+++ src/SortingAndSearch_cuda.cu
@@ -21,7 +21,7 @@
 extern __constant__
 SysParams system_params;
 
-struct particle_before
+struct cell_before
 {
 	__host__ __device__
 	bool operator()(const uint4 &left, const uint4 &right) const
@@ -31,6 +31,37 @@ struct particle_before
 	}
 };
 
+struct cell_equal
+{
+	__host__ __device__
+	bool operator()(const uint4 &left, const uint4 &right) const
+	{
+		return left.z == right.z && left.y == right.y && left.x == right.x;
+	}
+};
+
+
+__host__ __device__
+uint find_first(uint4 *dSortedGrid, const uint4 value, const uint n){
+	uint l = 0;
+	uint r = n - 1;
+
+	while( l <= r){
+		unint m = (l + r)/2;
+		if( cell_equal(dSortedGrid[m], value)){ // found, search for first
+			while(m > 0 && cell_equal(dSortedGrid[m-1], dSortedGrid[m]))
+				m--;
+			return m;
+
+		}else if(cell_before(dSortedGrid[m]), value){
+			l = m + 1;
+		}else{
+			r = m - 1;
+		}
+	}
+
+	return n;
+}
 
 
 __global__
@@ -64,7 +95,7 @@ void SortingAndSearch::prepareGrid(float4 *dPos){
 void SortingAndSearch::sortParticles(){
 	thrust::sort(thrust::device_ptr<uint4>(dSortedGrid),
 			thrust::device_ptr<uint4>(dSortedGrid + n_particles),
-			particle_before());
+			cell_before());
 }
 
 __global__
@@ -114,38 +145,28 @@ void calculate_contact_force(float4 *sortedPos, float4 *sortedVel,
 		for(int y = -1; y <= 1; y++){
 			// get index of first x-1 continue until x != x+1
 			
-			int x = gridPos.x-1 >=0 ? gridPos.x-1: gridPos.x;
-                        uint4 cell = make_uint4(x, gridPos.y + y, gridPos.z + z,0);
+			int x = gridPos.x > 0 ? gridPos.x-1: gridPos.x;
+			uint4 cell = make_uint4(x, gridPos.y + y, gridPos.z + z,0);
 			
 			if(cell.y >= gridDim.y || cell.z >= gridDim.z)
 				continue;
 
-			//thrust::device_ptr<uint4> start = thrust::lower_bound(thrust::device_ptr<uint4>(dSortedGrid),
-			//		thrust::device_ptr<uint4>(dSortedGrid + n_particles),
-			//		cell, particle_before());
-			//thrust::device_ptr<uint4> b = start;
-			//++b;
-//			for(int x = -1; x <= 1; x++)
-//			{
-//                                int3 cell = gridPos + make_int3(x,y,z);
-//
-//
-//				unsigned int hash =  makeHash(cell);
-//				unsigned int start_idx = cellStart[hash];
-//
-//				if(start_idx != EMPTY){
-//					unsigned int end_idx = cellEnd[hash];
-//
-//					for(unsigned int i=start_idx; i< end_idx; i++){
-//						if(i == idx) continue; // jumps self
-//						float3 neigh_pos = make_float3(sortedPos[i]);
-//						float3 neigh_vel = make_float3(sortedVel[i]);
-//
-//						resulting_force += World::contactForce(pos, neigh_pos,
-//								vel, neigh_vel, r, r);
-//					}
-//				}
-//			}
+			uint other = find_first(dSortedGrid,cell, n_particles);
+			while(other < n_particles){
+				uint4 neigh = dSortedGrid[other];
+				if(neigh.z != cell.z || neigh.y != cell.y || neigh.x > cell.x + 1){
+					// Check if next one is still in line (x-1 -> x+1, y, z)
+					break;
+				}
+				if(neigh.w != idx){
+					float3 neigh_pos = make_float3(sortedPos[other]);
+					float3 neigh_vel = make_float3(sortedVel[other]);
+
+					resulting_force += World::contactForce(pos, neigh_pos,
+							vel, neigh_vel, r, r);
+				}
+				other++;
+			}
 		}
 	}
 
